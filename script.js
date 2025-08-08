18cd42a3c6a577406a29c4af507518b6b60d988d
daffanu23 <daffanugraha058@gmail.com>
2025-08-08 08:28:45 +0700

    testing deploy + db

---
+++ script.js
@@ -0,0 +1,16 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabase = createClient(
+    "https://otwpcpfrcqogtcsnfxtu.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90d3BjcGZyY3FvZ3Rjc25meHR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2MTAwNzYsImV4cCI6MjA3MDE4NjA3Nn0.u5C3LjsJq6lkBpM5SDhjPV9rpn4JxldFpRtfXtGaHks"
+);
+
+    async function simpan() {
+        const name = document.getElementById("name").value;
+        const {data, error} = await supabase.from("portal_berita_protov2").insert([{name}])
+
+         if(error) return alert("Data gagal ditambahkan")
+            alert("Data berhasil di tambahkan")
+  }
+
+    window.simpan = simpan;
